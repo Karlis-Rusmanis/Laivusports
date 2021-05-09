06d31eceffa7799ce786f0ef4322d6453b1a2b96
Karlis-Rusmanis <rusmanis.karlis@gmail.com>
2021-05-09 12:58:36 +0300

    subject functionallity added

--- mail_handler.php
+++ mail_handler.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     $to = "rusmanis.karlis@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['firstname'];
-    $subject = "Form submission";
+    $subject = $_POST['topic'];
     $subject2 = "Jūsu ziņa";
     $message = $first_name . " " . " rakstīja:" . "\n\n" . $_POST['message'];
     $message2 = "Jūsu ziņa tika saņemta: " . $first_name . "\n\n" . $_POST['message'];
